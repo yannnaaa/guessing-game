423f42c1486b66f0a96942a8b81a1e7285fddee0
yannnaaa <yannnaaa@mail.ru>
2019-10-19 14:24:49 +0300

    add class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max) {
+	this.min = min;
+	this.max = max;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+	this.min = min;
+	this.max = max;
     }
 
     guess() {
-
+        this.result = this.min + Math.ceil((this.max-this.min)/2);
+	console.log(this.result);
+	return this.result;
     }
 
     lower() {
-
+	this.max = this.result;
     }
 
     greater() {
-
+	this.min = this.result;
     }
 }
 
